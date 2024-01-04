07834ad780252db06fcd61b956a0308fbed50449
vaggoban <mrg25mrg25@gmail.com>
2024-01-04 21:37:38 +0600

    d2

--- app/(pages)/about/page.js
+++ app/(pages)/about/page.js
@@ -1,11 +1,26 @@
 "use client"
 import {Component} from "react"
+import Personal from "../../(aboutPages)/personal/page"
+import Qualification from "../../(aboutPages)/Qualification/page"
+import Skills from "../../(aboutPages)/skills/page"
+
 class MyComponent extends Component{
   componentDidMount(){}
   render(){
     return(
       <>
       <p>dekha jak</p>
+      <div className="them">
+        <div className="personal">
+          <Personal/>
+        </div>
+        <div className="qualification">
+          <Qualification/>
+        </div>
+        <div className="skills">
+          <Skills/>
+        </div>
+      </div>
       </>
     )
   }
